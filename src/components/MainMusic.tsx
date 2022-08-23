d78c88acfdc42b602780d70c173a277657abe1bd
Animechill30 <82389963+AmerUkmata@users.noreply.github.com>
2022-08-24 01:01:26 +0200

    a

--- src/components/MainMusic.tsx
+++ src/components/MainMusic.tsx
@@ -1,24 +1,45 @@
 import { useState } from "react"
+
+const music = new Audio()
+
+function playMusic() {
+    if (music.paused) {
+        music.play()
+    } else {
+        music.pause()
+    }
+}
+
 export function MainMusic() {
-    const [music, setmusic] = useState([
-        {
-            "music": [
+    const [songs, setsongs] = useState([
+        
+             
                 {
-                    "id": 1,
+                    "title": "hello",
                     "url": "https://youtu.be/SAFHQobvqXI"
                 },
                 {
-                    "id": 1,
+                    "title": "gold",
                     "url": "https://youtu.be/SAFHQobvqXI"
                 },
                 {
-                    "id": 1,
+                    "title": "nice beat",
                     "url": "https://youtu.be/SAFHQobvqXI"
                 }
-            ]
-        }
     ])
+
     return (
-        <div>hello world</div>
+        <div>
+            {songs.map(song => (
+                <button
+                    onClick={() => {
+                        music.src = song.url
+                        playMusic()
+                    }}
+                >
+                    {song.title}
+                </button>
+            ))}
+        </div>
     )
 }
