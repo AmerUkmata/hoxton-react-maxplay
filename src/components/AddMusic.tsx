509c3d599d6b7a47a6c0fef28979a59dd1c68819
Animechill30 <82389963+AmerUkmata@users.noreply.github.com>
2022-08-25 16:54:56 +0200

    a

--- src/components/AddMusic.tsx
+++ src/components/AddMusic.tsx
@@ -9,17 +9,17 @@ export function AddMusic({ songs, setSongs }){
             title: event.target.title.value,
             source: event.target.url.value
         }
-        setSongs(...songs, song)
+        setSongs([...songs, song])
   }    
   
     //   console.log(songs) < = this should show all your songs in the console. if its undefined - DONT TOUCH FUNCTIONS. FIX PROPS FIRST!
   
       return(
        <div>
-          <form>
+          <form onSubmit={addSong}>
           <input name="title" type="text" required />
           <input name="url" type="url" pattern=".*\.mp3$" required />
-          <button onClick={addSong}>ADD SONG</button>
+          <button >ADD SONG</button>
         </form>
        </div>
       )
