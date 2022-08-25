d08e0d3bbacf5f46ffe8e44b398a0bb5c3a5ed04
Animechill30 <82389963+AmerUkmata@users.noreply.github.com>
2022-08-25 13:33:42 +0200

    a

--- src/components/AddMusic.tsx
+++ src/components/AddMusic.tsx
@@ -1,14 +1,13 @@
 // import App from "../App"
 
-function getMusic(){
-    fetch('need')
-    .then(resp => resp.json)
-    .then()
-}
-
 export function AddMusic({ songs, setSongs }){
     function addSong(){
-      // we will build this soon  
+      const newMusic : Songs = {
+        id: Math.random(),
+        title: "titles",
+        source: "urls"
+      }
+      songs.push(newMusic)
   }    
   
     //   console.log(songs) < = this should show all your songs in the console. if its undefined - DONT TOUCH FUNCTIONS. FIX PROPS FIRST!
@@ -16,8 +15,8 @@ export function AddMusic({ songs, setSongs }){
       return(
        <div>
           <form>
-          <input type="text" required />
-          <input type="url" pattern=".*\.mp4$" required />
+          <input className="titles" type="text" required />
+          <input className="urls" type="url" pattern=".*\.mp3$" required />
           <button onClick={addSong}>ADD SONG</button>
         </form>
        </div>
